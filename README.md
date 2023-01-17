558d988466578803ed1d3f9c210c2425720decad
kootsZhin <69758390+kootsZhin@users.noreply.github.com>
2023-01-17 09:41:21 +0800

    feat: remote script

---
+++ README.md
@@ -0,0 +1,5 @@
+# use-cairo
+
+```bash
+curl -L https://www.kootszhin.xyz/use-cairo/install | bash
+```
--- setup-cairo.sh
+++
@@ -1,7 +0,0 @@
-python3.9 -m venv ~/cairo_venv
-source ~/cairo_venv/bin/activate
-
-pip3 install ecdsa fastecdsa sympy cairo-lang
-
-echo 'alias use-cairo="source ~/cairo_venv/bin/activate"' >> ~/.zshrc
-source .zshrc
